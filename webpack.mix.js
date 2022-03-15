1b1f383407047305a5bef2a9657a4b9d1059d42b
Aleksei Alekseev <axlle@axlle.com>
2022-03-15 14:57:19 +0300

    init->composer->webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,9 +4,9 @@ mix.styles([
     'resources/frontend/plugins/bootstrap/css/bootstrap.css',
     'resources/frontend/css/main.css',
     'resources/frontend/css/common.css',
-], 'assets/frontend/css/main.min.css');
-mix.js('resources/frontend/js/app.js','assets/frontend/js/').extract(['jquery', 'bootstrap',]);
+], 'resources/assets/frontend/css/main.min.css');
+mix.js('resources/frontend/js/app.js','resources/assets/frontend/js/').extract(['jquery', 'bootstrap',]);
 mix.scripts([
     'resources/frontend/js/common.js',
-], 'assets/frontend/js/main.min.js');
-mix.copy('resources/frontend/images', 'assets/frontend/images');
+], 'resources/assets/frontend/js/main.min.js');
+mix.copy('resources/frontend/images', 'resources/assets/frontend/images');
