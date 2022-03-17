71ae227a8f652bb1322717027bd5630867109fb1
Aleksei Alekseev <axlle@axlle.com>
2022-03-17 12:27:19 +0300

    fix img

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -40,7 +40,7 @@
     <footer class="pt-4 my-md-5 pt-md-5 border-top">
         <div class="row">
             <div class="col-12 col-md">
-                <img class="mb-2" src="/assets/frontend/images/bootstrap-solid.svg" alt="" width="24" height="24">
+                <img class="mb-2" src="<?= axAssetsFrontend('/images/bootstrap-solid.svg') ?>" alt="" width="24" height="24">
                 <small class="d-block mb-3 text-muted">&copy; 2017-2021</small>
             </div>
             <div class="col-6 col-md">
